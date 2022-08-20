8b66d1077e54073a025806b8473c893dd8adf44a
zoleak <36116239+zoleak@users.noreply.github.com>
2022-08-20 18:15:37 -0400

    first commit

---
+++ server.R
@@ -0,0 +1,383 @@
+# Load Packages
+library(shiny)
+library(rmarkdown)
+library(ggplot2)
+
+# Define server logic 
+shinyServer(function(input, output,session) {
+  
+# Updates loan amount based on purchase price input
+  observe({
+    req(input$purchase)
+    req(input$down)
+    loan_amount<- input$purchase*(1-input$down/100)
+    
+    updateNumericInput(session,"loan",value=loan_amount)
+  })
+#Interest amount
+  observe({
+    req(input$loan)
+    req(input$loan_term)
+    req(input$rate)
+    req(input$payments_per)
+    
+    interest_dollar<-round((input$rate/100)/input$payments_per*input$loan)
+    
+    updateNumericInput(session,"inter",value=interest_dollar)
+    
+    
+  })
+#Principal amount
+  observe({
+    req(input$loan)
+    req(input$loan_term)
+    req(input$rate)
+    req(input$payments_per)
+    
+    p= input$loan
+    j=input$rate/(12*100)
+    L=input$loan_term
+    N= L*12
+    
+    estimated_pay<-round(p*(j/(1-(1+j)^-N)),digits = 2)
+    
+    interest_dollar<-round((input$rate/100)/input$payments_per*input$loan)
+    
+    principal_dollar<- round(estimated_pay-interest_dollar)
+    
+    updateNumericInput(session,"princ",value=principal_dollar)
+    
+    
+  })
+# Gets PMI amount based on % of PMI rate  
+  observe({
+    req(input$pmi)
+    req(input$loan)
+    pmi_show<- round(input$loan*(input$pmi/100/12),digits = 2)
+    
+    updateNumericInput(session,"pmi_dollar",value=pmi_show)
+  })
+# Gets rent total 
+  output$output <- renderText({
+    paste("$",input$unit1+input$unit2+input$unit3+input$unit4+input$unit5)
+  })
+# Gets other income total 
+  output$output2 <- renderText({
+    paste("$",input$pet+input$garage+input$storage+input$utility_fee+input$other)
+  })
+# Gets total gross income   
+  output$output3 <- renderText({
+    req(input$unit1)
+    req(input$unit2)
+    req(input$unit3)
+    req(input$unit4)
+    req(input$unit5)
+    req(input$pet)
+    req(input$garage)
+    req(input$storage)
+    req(input$utility_fee)
+    req(input$other)
+
+   paste("$",total_rent<-input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+           input$pet+input$garage+input$storage+input$utility_fee+input$other)
+    
+  })
+# Estimates monthly mortgage payment  
+  output$debt_payment<-renderText({
+    req(input$loan)
+    req(input$rate)
+    req(input$loan_term)
+    req(input$taxes)
+    req(input$insurance)
+    req(input$pmi_dollar)
+    
+    p= input$loan
+    j=input$rate/(12*100)
+    L=input$loan_term
+    N= L*12
+    
+    paste("$",round(round(p*(j/(1-(1+j)^-N)),digits = 2)+input$taxes+input$insurance+input$pmi_dollar))
+
+    })
+# Gets total reserves
+  output$output_reserves <- renderText({
+    paste("$",(input$repairs_mait/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$vacancy/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$capex/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$management/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other))
+    
+  })
+  
+# Reactive UI based on action button 
+  observeEvent(input$extra_ex, {
+    insertUI(
+      selector = "#extra_ex",
+      where = "beforeBegin",
+      ui = numericInput("txt", "Other",input$extra_ex,
+                     0)
+    )
+  })
+# Gets total expenses  
+  output$total_expenses<-renderText({
+    req(input$loan)
+    req(input$rate)
+    req(input$loan_term)
+    req(input$taxes)
+    req(input$insurance)
+    req(input$pmi_dollar)
+    
+    p= input$loan
+    j=input$rate/(12*100)
+    L=input$loan_term
+    N= L*12
+    
+   paste("$",round(round(p*(j/(1-(1+j)^-N)),digits = 2)+input$taxes+input$insurance+input$pmi_dollar+
+      (input$repairs_mait/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$vacancy/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$capex/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$management/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)))
+  })
+  
+# Creates box showing cashflow
+  output$cashflow <- renderInfoBox({
+    req(input$loan)
+    req(input$rate)
+    req(input$loan_term)
+    req(input$taxes)
+    req(input$insurance)
+    req(input$pmi_dollar)
+     
+    p= input$loan
+    j=input$rate/(12*100)
+    L=input$loan_term
+    N= L*12
+    
+    te<-round(p*(j/(1-(1+j)^-N)),digits = 2)+input$taxes+input$insurance+input$pmi_dollar+
+      (input$repairs_mait/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$vacancy/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+                                                                                                                  input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$capex/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+         input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$management/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+         input$pet+input$garage+input$storage+input$utility_fee+input$other)
+  
+    ti<- input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other
+    
+    cf<-round((ti-te))
+    if(cf>0){
+     
+    infoBox("Cash Flow:",value = paste0("$",cf),color = "green",
+            fill = T,icon =shiny::icon("hand-holding-usd"))}
+    
+    else{
+    
+      infoBox("Cash Flow:",value = paste0("$",cf),color = "red",
+              fill = T,icon =shiny::icon("hand-holding-usd"))
+    }
+    
+  })
+# Box to show down payment 
+  output$dp<-renderInfoBox({
+    
+    dp<-round(input$purchase*(input$down/100))
+    
+    infoBox("Down Payment:",value = paste0("$",dp),color = "blue",
+            fill = T,icon =shiny::icon("coins"))
+  })
+# Box to show initial investment 
+  output$all_in<-renderInfoBox({
+    
+    dp<-round(input$purchase*(input$down/100))
+    
+    inital_investment<-round(dp+input$closing+input$rehab_costs)
+    
+    infoBox("Initial Investment:",value = paste0("$",inital_investment),
+            color = "blue",icon =shiny::icon("coins"),
+            fill = T)
+  })
+# Box to show cash on cash return on investment
+  output$coc <- renderInfoBox({
+    p= input$loan
+    j=input$rate/(12*100)
+    L=input$loan_term
+    N= L*12
+    
+    te<-round(p*(j/(1-(1+j)^-N)),digits = 2)+input$taxes+input$insurance+input$pmi_dollar+
+      (input$repairs_mait/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+                                  input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$vacancy/100)*(input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+                                                                                                                  input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$capex/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+         input$pet+input$garage+input$storage+input$utility_fee+input$other)+(input$management/100)*
+      (input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+         input$pet+input$garage+input$storage+input$utility_fee+input$other)
+    
+    ti<-input$unit1+input$unit2+input$unit3+input$unit4+input$unit5+
+      input$pet+input$garage+input$storage+input$utility_fee+input$other
+    
+    dp<-input$purchase*(input$down/100)
+    
+    total_cash<-round(input$closing+input$rehab_costs+dp)
+    
+    annual_cashflow<-round((ti-te)*12)
+    
+    cashoncash<-round((annual_cashflow/total_cash)*100)
+    if(cashoncash>0){
+    infoBox("CoC RoI:",value = paste0(cashoncash,"%"),color = "green",
+            fill = T)}
+    
+    else{
+      infoBox("CoC RoI:",value = paste0(cashoncash,"%"),color = "red",
+              fill = T)
+    }
+  })
+# Creates download button to download report from calculations,etc.
+  file1 <- reactive({gsub("\\\\", "/", input$upload$datapath)})
+  
+  output$downloadReport <- downloadHandler(
+    filename = 
+      paste("report_file","file",".html",sep=""),
+    content = function(file) {
+      tempReport<-file.path(tempdir(),"report_file.Rmd")
+      file.copy('report_file.Rmd', tempReport,overwrite = TRUE)
+      ##Parameters to pass
+      params <- list(text1=input$descp,pic1=file1(),text2=input$address,
+                     Unit1=input$unit1,Unit2=input$unit2,Unit3=input$unit3,
+                     Unit4=input$unit4,Unit5=input$unit5,Pet=input$pet,Garage=input$garage,
+                     Storage=input$storage,Utility=input$utility_fee,Other=input$other,
+                     Principal = input$princ,Interest = input$inter,Taxes=input$taxes,
+                     Insurance = input$insurance,PMI=input$pmi_dollar,Repairs = input$repairs_mait,
+                     Vacancy = input$vacancy,Capex=input$capex, Mait=input$management,
+                     Loan = input$loan, Rate = input$rate,Loan_term=input$loan_term,
+                     Purchase = input$purchase,Down = input$down,Closing=input$closing,
+                     Rehab = input$rehab_costs)
+      rmarkdown::render(tempReport,output_file=file, params=params,
+                        envir = new.env(parent = globalenv()))
+    }  
+  )
+  
+  mortgage <- function(P = 500000, I = 6, L = 30, amort = TRUE, plotData = TRUE) {
+    J <- I / (12 * 100)
+    N <- 12 * L
+    M <- P * J / (1 - (1 + J)^(-N))
+    monthPay <<- M
+    # Calculate Amortization for each Month
+    if (amort == TRUE) {
+      Pt <- P # current principal or amount of the loan
+      currP <- NULL
+      while (Pt >= 0) {
+        H <- Pt * J # this is the current monthly interest
+        C <- M - H # this is your monthly payment minus your monthly interest, so it is the amount of principal you pay for that month
+        Q <- Pt - C # this is the new balance of your principal of your loan
+        Pt <- Q # sets P equal to Q and goes back to step 1. The loop continues until the value Q (and hence P) goes to zero
+        currP <- c(currP, Pt)
+      }
+      monthP <- c(P, currP[1:(length(currP) - 1)]) - currP
+      aDFmonth <<- data.frame(
+        Month = 1:length(currP),
+        Year = sort(rep(1:ceiling(N / 12), 12))[1:length(monthP)],
+        Balance = c(currP[1:(length(currP))]),
+        Payment = monthP + c((monthPay - monthP)[1:(length(monthP))]),
+        Principal = monthP,
+        Interest = c((monthPay - monthP)[1:(length(monthP))])
+      )
+      aDFmonth <<- subset(aDFmonth, Year <= L * 12)
+      aDFyear <- data.frame(
+        Amortization = tapply(aDFmonth$Balance, aDFmonth$Year, max),
+        Annual_Payment = tapply(aDFmonth$Payment, aDFmonth$Year, sum),
+        Annual_Principal = tapply(aDFmonth$Principal, aDFmonth$Year, sum),
+        Annual_Interest = tapply(aDFmonth$Interest, aDFmonth$Year, sum),
+        Year = as.factor(na.omit(unique(aDFmonth$Year)))
+      )
+      aDFyear <<- aDFyear
+    }
+    if (plotData == TRUE) {
+      aDFyear2 <- aDFyear %>%
+        rename(
+          Interest = Annual_Interest,
+          Payment = Annual_Payment,
+          Principal = Annual_Principal
+        )
+      aDFyear2$Year <- as.factor(aDFyear2$Year)
+      aDFyear2 <- melt(aDFyear2[, c("Interest", "Principal", "Year")], id.vars = "Year")
+      
+      ggplot(aDFyear2, aes(x = Year, y = value, fill = variable)) +
+        geom_bar(position = "fill", stat = "identity") +
+        labs(y = "Payment") +
+        scale_y_continuous(labels = percent) +
+        theme_minimal() +
+        theme(legend.title = element_blank(), legend.position = "top")
+    }
+  }
+  
+  output$text <- renderUI({
+    mortgage(P = input$principal, I = input$interest, L = input$length, plotData = FALSE)
+    HTML(paste0(
+      "<h3>", "Summary", "</h3>",
+      "Principal (loan amount): ", format(round(input$principal, 2), big.mark = ","),
+      "<br>",
+      "Annual interest rate: ", input$interest, "%",
+      "<br>",
+      "Term: ", input$length, " years (", input$length * 12, " months)",
+      "<br>",
+      "<b>", "Monthly payment: ", format(round(monthPay, digits = 2), big.mark = ","), "</b>",
+      "<br>",
+      "<b>", "Total cost: ", "</b>", format(round(input$principal, 2), big.mark = ","), " (principal) + ", format(round(monthPay * 12 * input$length - input$principal, 2), big.mark = ","), " (interest) = ", "<b>", format(round(monthPay * 12 * input$length, digits = 2), big.mark = ","), "</b>"
+    ))
+  })
+  
+  output$distPlot <- renderPlot({
+    mortgage(P = input$principal, I = input$interest, L = input$length, plotData = input$plot)
+  })
+  
+  # Data output
+  output$tbl <- DT::renderDataTable({
+    mortgage(P = input$principal, I = input$interest, L = input$length, plotData = FALSE)
+    df_month <- DT::datatable(data.frame(round(aDFmonth, 2)),
+                              extensions = "Buttons",
+                              options = list(
+                                lengthChange = TRUE,
+                                dom = "Blrtip",
+                                buttons = c("copy", "csv", "excel", "pdf", "print"),
+                                
+                                lengthMenu = list(c(-1, 10, 12, 15, 25, 50, 100), c("All", "10", "12", "15", "25", "50", "100"))
+                              ),
+                              rownames = FALSE
+    ) %>%
+      formatCurrency(c("Balance", "Payment", "Principal", "Interest"), currency = "", interval = 3, mark = ",")
+  })
+  
+  #output$plot1<-renderPlot({
+  #  df<-data.frame("Year"=seq(input$loan_term),"Rent"=(input$unit1*(input$rent_increase/100))+input$unit*input$loan_term)
+ #   print(df)
+#    ggplot(data=df)+geom_point(aes(x=Year,y=Rent))
+#  })
+  
+#  output$plot2<-renderPlot({
+#    # Create Data
+#    data <- data.frame(
+#      group=c("PITI","CapEx","Repairs/Maintenance",
+#              "Vacancy","Management","PMI"),
+#      value=c((input$taxes+input$insurance+input$princ+input$inter),input$capex,
+#              input$repairs_mait,input$vacancy,input$management,input$pmi_dollar)
+#    )
+#    
+#    # Basic piechart
+#    ggplot(data, aes(x="", y=value, fill=group)) +
+#      geom_bar(stat="identity", width=1) +
+#      coord_polar("y", start=0)+
+#      theme_void()+
+#      labs(fill = "Expenses")+
+#      scale_fill_brewer(palette="Set1")
+    
+  #})
+  
+})
+
+
+
+
---
+++ ui.R
@@ -0,0 +1,189 @@
+# Script to make real estate investing calculator to analyze deals
+# Author: Kevin Zolea
+# Date: 4/24/20
+
+library(shiny)
+library(shinyWidgets)
+library(shinythemes)
+library(rmarkdown)
+library(shinydashboard)
+library(shinycssloaders)
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+library(scales)
+library(DT)
+options(scipen = 999)
+# Define UI for application 
+shinyUI(navbarPage(theme = shinytheme('superhero'),
+                   tags$b("Rental Property Calculator"),
+                   windowTitle = "Rental Calc",
+###### Here : insert shinydashboard dependencies ######
+                   header = tagList(
+                     useShinydashboard()
+                   ),
+ #######################################################
+  tabPanel("Property Calculations",
+           tags$head(
+             includeCSS("www/styles.css")),
+           fluidRow(
+             column(width= 3,
+              div(style= "text-align:center",h3("Property Info"))),
+             column(width = 3,
+             textInput("address","Address:",placeholder = "35 Wilson Ave Parlin NJ 08859")),
+             column(width = 3,
+            textInput("descp","Description:",placeholder = "This duplex style colonial has all the 
+                      amenities you can ask for and is within walking distance to the 
+                      downtown shops and restaurants.")),
+            column(width =3,
+            fileInput("upload", "Upload image:", accept = c("image/png",
+                                                            "image/jpeg",
+                                                            "image/pdf")))),hr(),
+  column(width = 3,
+         h3("Purchase Info"),
+         numericInputIcon("purchase",
+                   "Purchase Price:",170000,width = 200,
+                   icon = icon("dollar")),
+       numericInputIcon("closing","Purchase closing costs:",6000,width = 200,
+                        icon = icon("dollar")),
+       numericInputIcon("rehab_costs","Estimated Rehab Costs:",10000,width = 200,
+                        icon = icon("dollar")),
+       numericInputIcon("arv","Estimated ARV:",175000,width = 200,
+                        icon = icon("dollar")),
+       numericInputIcon("appreciation","Estimated Annual Appreciation:",3,width = 200,
+                        icon= list(NULL, icon("percent"))),
+       numericInputIcon("rent_increase","Annual Rent Increase:",2,width = 200,
+                        icon= list(NULL, icon("percent"))),
+    h3("Mortgage Details"),
+    numericInputIcon(
+      inputId = "down",
+      label = "Down Payment:",
+      value = 5,
+      width = 200,
+      icon= list(NULL, icon("percent"))),
+    numericInputIcon("loan","Loan Amount:",NULL,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("rate","Interest Rate:",3.625,width = 200,
+                     icon= list(NULL, icon("percent"))),
+    numericInputIcon("pmi","PMI Rate:",1,width = 200,
+                     icon= list(NULL, icon("percent"))),
+    numericInputIcon("loan_term","Loan Term in Years:",30,width = 200),
+    numericInputIcon("payments_per","Payments Made Per Year:",12,width = 200)),
+  column(width = 3,
+    h3("Rental Income"),
+    numericInputIcon("unit1","Unit 1:",1200,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("unit2","Unit 2:",1000,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("unit3","Unit 3:",0,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("unit4","Unit 4:",0,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("unit5","Unit 5:",0,width = 200,
+                     icon = icon("dollar")),
+    "Subtotal Estimated Rent:",
+    textOutput("output"),br(),br(),br(),
+    infoBoxOutput("dp",width = 12),
+    infoBoxOutput("coc",width = 12)),
+  column(width = 3,
+    h3("Other Income"),
+    numericInputIcon("pet","Pet Rent:",0,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("garage","Garage Parking:",0,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("storage","Extra Storage:",0,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("utility_fee","Utility Fee:",0,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("other","Other:",0,width = 200,
+                     icon = icon("dollar")),
+    "Subtotal Other Income:",
+    textOutput("output2"),
+    "Total Monthly Income:",
+    textOutput("output3"),br(),
+    infoBoxOutput("all_in",width = 12),
+    infoBoxOutput("cashflow",width = 12),
+    downloadBttn(
+      'downloadReport',
+      label = "Download Report",
+      style = "unite",
+      color = "default",
+      size = "lg",
+      block = FALSE,
+      no_outline = TRUE)),
+  column(width=3,
+    h3("Property Expenses"),
+    numericInputIcon("princ","Principal:",NULL,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("inter","Interest:",450,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("taxes","Estimated Monthly Taxes:",450,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("insurance","Estimated Monthly Insurance:",80,width = 200,
+                     icon = icon("dollar")),
+    numericInputIcon("pmi_dollar","PMI:",NULL,width = 200,
+                     icon= icon("dollar")),
+    actionBttn("extra_ex",label = "Add Expense:"),
+    #uiOutput("ui"),
+    br(),
+    "Subtotal Monthly Payment:",
+    textOutput("debt_payment"),br(),
+    em("*Repairs & Maintenance, vacancy, capital expenditures, 
+    and management fees are expressed as percentages 
+    of total monthly income."),br(),br(),
+    numericInputIcon("repairs_mait","Repairs & Maintenance:",7,width = 200,
+                     icon= list(NULL, icon("percent"))),
+    numericInputIcon("vacancy","Vacancy:",7,width = 200,
+                     icon= list(NULL, icon("percent"))),
+    numericInputIcon("capex","Capital Expenditures:",7,width = 200,
+                     icon= list(NULL, icon("percent"))),
+    numericInputIcon("management","Management Fees:",10,width = 200,
+                     icon= list(NULL, icon("percent"))),
+    "Subtotal Reserves:",
+    textOutput("output_reserves"),br(),
+    "Total Monthly Expenses:",
+  textOutput("total_expenses"))),
+tabPanel("Mortgage Calculations",
+         # Sidebar layout with input and output definitions ----
+         sidebarLayout(
+           
+           # Sidebar to demonstrate various slider options ----
+           sidebarPanel(
+             
+             # Input: Simple integer interval ----
+             numericInput("principal", "Principal (loan amount)", 200000, min = 0, step = 1000),
+             hr(),
+             numericInput("interest", "Interest rate (in %)", 2, min = 0, max = 100, step = 0.01),
+             hr(),
+             sliderInput("length", "Duration of the loan (in years)",
+                         min = 0,
+                         max = 30,
+                         value = 25,
+                         step = 1
+             ),
+             hr(),
+             checkboxInput("plot", "Display plot?", TRUE)
+           ),
+           
+           # Main panel for displaying outputs ----
+           mainPanel(
+             
+             # Output: Table summarizing the values entered ----
+             uiOutput("text"),
+             br(),
+             plotOutput("distPlot"),
+             br(),
+             DT::dataTableOutput("tbl")
+            
+           )
+         )
+#  tabPanel("Plots",
+#    fluidRow(column(6,
+#    plotOutput("plot1",6),
+#    plotOutput("plot2")%>%withSpinner(type = 5, color = "red"))
+#    
+#  ))
+)))
+
+
+
